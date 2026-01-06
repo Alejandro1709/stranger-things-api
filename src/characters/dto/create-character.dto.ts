2fa20cf72dbf2cd782e02723698c5a7cc0e9b6ba
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-01-06 15:10:13 -0500

    Add: Character create dto and add class-transformer and class-validator

--- src/characters/dto/create-character.dto.ts
+++ src/characters/dto/create-character.dto.ts
@@ -1,1 +1,33 @@
-export class CreateCharacterDto {}
+import {
+  IsArray,
+  IsIn,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateCharacterDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  actor: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsIn(['vivo', 'muerto', 'desconocido'], {
+    message: 'Status debe ser vivo, muerto o desconocido',
+  })
+  status: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  abilities?: string[];
+}
