fbfc9cb5a23ba60d9ad9f8df6d89a96e00eabf09
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-01-06 16:19:40 -0500

    Add: location resources and add entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CharactersModule } from './characters/characters.module';
+import { LocationsModule } from './locations/locations.module';
 
 @Module({
-  imports: [CharactersModule],
+  imports: [CharactersModule, LocationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
