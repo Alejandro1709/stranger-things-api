89e7532bd0ddaa5a8a553befaffd94d089ea39a7
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-01-06 14:56:30 -0500

    Add: characters data

---
+++ src/data/characters.data.ts
@@ -0,0 +1,98 @@
+export const charactersData = [
+  {
+    id: 1,
+    name: 'Eleven / Jane Hopper',
+    actor: 'Millie Bobby Brown',
+    description:
+      'Una joven con habilidades psíquicas y telequinéticas que escapó del Laboratorio Nacional de Hawkins. Adoptada por Jim Hopper.',
+    status: 'Vivo',
+    abilities: ['Telequinesis', 'Proyección astral', 'Manipulación sensorial'],
+  },
+  {
+    id: 2,
+    name: 'Mike Wheeler',
+    actor: 'Finn Wolfhard',
+    description:
+      'Mejor amigo de Will y novio de Eleven. Líder natural del grupo y estratega del Club Hellfire.',
+    status: 'Vivo',
+  },
+  {
+    id: 3,
+    name: 'Dustin Henderson',
+    actor: 'Gaten Matarazzo',
+    description:
+      'El mediador del grupo con un gran amor por la ciencia. Tiene una relación a distancia con Suzie.',
+    status: 'Vivo',
+  },
+  {
+    id: 4,
+    name: 'Lucas Sinclair',
+    actor: 'Caleb McLaughlin',
+    description:
+      'Miembro del grupo conocido por su pragmatismo. Jugador de baloncesto y novio de Max.',
+    status: 'Vivo',
+  },
+  {
+    id: 5,
+    name: 'Will Byers',
+    actor: 'Noah Schnapp',
+    description:
+      'Primera víctima del Demogorgon. Tiene una conexión especial con el Mundo del Revés.',
+    status: 'Vivo',
+  },
+  {
+    id: 6,
+    name: 'Max Mayfield',
+    actor: 'Sadie Sink',
+    description:
+      'Nueva en Hawkins, skater y amante de los videojuegos. Sobreviviente de los ataques de Vecna.',
+    status: 'Vivo',
+  },
+  {
+    id: 7,
+    name: 'Steve Harrington',
+    actor: 'Joe Keery',
+    description:
+      "Ex-rey de la escuela convertido en 'niñera' del grupo. Trabaja en Scoops Ahoy y después en Family Video.",
+    status: 'Vivo',
+  },
+  {
+    id: 8,
+    name: 'Nancy Wheeler',
+    actor: 'Natalia Dyer',
+    description:
+      'Hermana mayor de Mike y periodista investigadora. Determinada a descubrir la verdad sobre Hawkins.',
+    status: 'Vivo',
+  },
+  {
+    id: 9,
+    name: 'Jonathan Byers',
+    actor: 'Charlie Heaton',
+    description: 'Hermano mayor de Will y fotógrafo talentoso. Novio de Nancy.',
+    status: 'Vivo',
+  },
+  {
+    id: 10,
+    name: 'Jim Hopper',
+    actor: 'David Harbour',
+    description:
+      'Jefe de policía de Hawkins y padre adoptivo de Eleven. Sobreviviente de prisión rusa.',
+    status: 'Vivo',
+  },
+  {
+    id: 11,
+    name: 'Joyce Byers',
+    actor: 'Winona Ryder',
+    description:
+      'Madre de Will y Jonathan. Incansable protectora de sus hijos y pareja de Hopper.',
+    status: 'Vivo',
+  },
+  {
+    id: 12,
+    name: 'Billy Hargrove',
+    actor: 'Dacre Montgomery',
+    description:
+      'Hermanastro de Max. Sacrificó su vida para salvar a Eleven del Azotamentes.',
+    status: 'Muerto',
+  },
+];
