fbeb31853432404e6bc9c205246c90da33925f68
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-01-06 17:08:46 -0500

    Add: seasons CRUD

--- src/seasons/seasons.service.ts
+++ src/seasons/seasons.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateSeasonDto } from './dto/create-season.dto';
 import { UpdateSeasonDto } from './dto/update-season.dto';
+import { Season } from './entities/season.entity';
+import { seasonsData } from 'src/data/seasons.data';
 
 @Injectable()
 export class SeasonsService {
+  private seasons: Season[] = structuredClone(seasonsData);
+
   create(createSeasonDto: CreateSeasonDto) {
-    return 'This action adds a new season';
+    const newSeason: Season = {
+      id: this.seasons.length + 1,
+      ...createSeasonDto,
+    };
+
+    this.seasons.push(newSeason);
+
+    return newSeason;
   }
 
   findAll() {
-    return `This action returns all seasons`;
+    return this.seasons;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} season`;
+    const season = this.seasons.find((season) => season.id === id);
+
+    if (!season) {
+      throw new NotFoundException('Season not found');
+    }
+
+    return season;
   }
 
   update(id: number, updateSeasonDto: UpdateSeasonDto) {
-    return `This action updates a #${id} season`;
+    const season = this.findOne(id);
+
+    return {
+      ...season,
+      ...updateSeasonDto,
+    };
   }
 
   remove(id: number) {
-    return `This action removes a #${id} season`;
+    const season = this.findOne(id);
+
+    this.seasons = this.seasons.filter((s) => s.id !== id);
+
+    return season;
   }
 }
