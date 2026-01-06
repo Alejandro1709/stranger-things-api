22cbdf45a62c2bceb8432d1325f6cfb93106096b
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-01-06 16:53:13 -0500

    Add: seasons resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CharactersModule } from './characters/characters.module';
 import { LocationsModule } from './locations/locations.module';
+import { SeasonsModule } from './seasons/seasons.module';
 
 @Module({
-  imports: [CharactersModule, LocationsModule],
+  imports: [CharactersModule, LocationsModule, SeasonsModule],
   controllers: [AppController],
   providers: [AppService],
 })
