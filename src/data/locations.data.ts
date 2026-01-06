ac5ebe29bf9c6b31c22c13292c8b7792f069a55c
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-01-06 16:17:36 -0500

    Add: locations data

---
+++ src/data/locations.data.ts
@@ -0,0 +1,86 @@
+export const locations = [
+  {
+    id: 1,
+    name: 'Hawkins, Indiana',
+    type: 'Ciudad',
+    description:
+      'Un pequeño pueblo estadounidense que se convierte en el epicentro de eventos sobrenaturales. Hogar de los protagonistas y el Laboratorio Nacional de Hawkins.',
+  },
+  {
+    id: 2,
+    name: 'El Mundo del Revés',
+    type: 'Dimensión Paralela',
+    description:
+      'Una dimensión oscura y tóxica que es un reflejo distorsionado de Hawkins. Hogar de criaturas hostiles y el Azotamentes.',
+  },
+  {
+    id: 3,
+    name: 'Laboratorio Nacional de Hawkins',
+    type: 'Instalación Gubernamental',
+    description:
+      'Instalación secreta del Departamento de Energía donde se realizaron experimentos con niños con habilidades psíquicas, incluyendo Eleven.',
+  },
+  {
+    id: 4,
+    name: 'Escuela Secundaria de Hawkins',
+    type: 'Escuela',
+    description:
+      'El lugar donde los personajes principales asisten a clases y donde ocurren varios eventos importantes de la serie.',
+  },
+  {
+    id: 5,
+    name: 'La Cabaña de Hopper',
+    type: 'Residencia',
+    description:
+      'Refugio aislado en el bosque donde Hopper escondió a Eleven durante la segunda temporada. Más tarde se convierte en su hogar familiar.',
+  },
+  {
+    id: 6,
+    name: 'Centro Comercial Starcourt',
+    type: 'Centro Comercial',
+    description:
+      'Moderno centro comercial inaugurado en 1985 que oculta una base secreta rusa en sus instalaciones subterráneas.',
+  },
+  {
+    id: 7,
+    name: 'La Casa de los Byers',
+    type: 'Residencia',
+    description:
+      'Hogar de Joyce, Will y Jonathan. El primer lugar donde se manifestaron las comunicaciones entre el mundo real y el Mundo del Revés.',
+  },
+  {
+    id: 8,
+    name: 'Casa Creel',
+    type: 'Mansión Abandonada',
+    description:
+      'Mansión embrujada donde ocurrieron asesinatos en los años 50. Hogar de Victor Creel y su familia, conectada con el origen de Vecna.',
+  },
+  {
+    id: 9,
+    name: 'Arcade Palace',
+    type: 'Salón Recreativo',
+    description:
+      'Salón de videojuegos donde los chicos pasan su tiempo libre. Lugar importante para varios eventos de la segunda temporada.',
+  },
+  {
+    id: 10,
+    name: 'Scoops Ahoy',
+    type: 'Heladería',
+    description:
+      'Heladería en Starcourt Mall donde Steve y Robin trabajan durante el verano de 1985. Punto de partida para descubrir la conspiración rusa.',
+  },
+  {
+    id: 11,
+    name: 'Kamchatka, Rusia',
+    type: 'Prisión',
+    description:
+      'Prisión soviética donde Hopper estuvo cautivo después de la explosión en Starcourt. Lugar donde los rusos continúan experimentando con el Mundo del Revés.',
+  },
+  {
+    id: 12,
+    name: "Lovers' Lake",
+    type: 'Lago',
+    description:
+      'Lago en las afueras de Hawkins que se convierte en un punto de acceso importante al Mundo del Revés en la cuarta temporada.',
+  },
+];
